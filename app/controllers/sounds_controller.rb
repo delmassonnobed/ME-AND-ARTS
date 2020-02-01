463b0d9330a18eaf0937b9cac95bdc6f57ee0b38
jeremy bultel <jerem.bultel@gmail.com>
2020-02-01 16:38:50 +0100

    presque ça

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -4,10 +4,24 @@ def index
   @sounds = Sound.all
 end
 
+def new
+  @sound = Sound.new
+end
+
+def create
+ @sound = Sound.new(sound_params)
+ @sound.user = current_user
+  if @sound.save
+    redirect_to sound_path(@sound)
+  else
+    render :new
+  end
+end
 
 
+private
 
-
-
-
+def sound_params
+  params.require(:sound).permit(:title, :price, :user_id)
+end
 end

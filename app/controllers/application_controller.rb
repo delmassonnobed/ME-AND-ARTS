c0a9e02258b79dfcbc57421ea9db6d14d71a1ddf
jacky jean louis <j.st.jeanlouis@gmail.com>
2020-02-08 18:19:07 +0100

    checkboxbuyer&back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    update_attrs = [:beatmaker, :email, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: update_attrs
+    # devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+    # devise_parameter_sanitizer.permit :accept_invitation, keys: update_attrs
+  end
 end

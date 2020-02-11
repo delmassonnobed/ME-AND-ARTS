fe14f718e88f65585dc6bc81a387b39ff9c40b51
jeremy bultel <jerem.bultel@gmail.com>
2020-02-11 20:15:57 +0100

    auction index + show + linktoshow

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'sounds#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
       resources :sounds do
-       resources :auctions
+       resources :auctions, only: [:new, :create]
       end
+      resources :auctions, only: [:index, :show]
+
 end

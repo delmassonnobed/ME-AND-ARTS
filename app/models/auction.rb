20cbee02079ff76033d39bd690c1d299c03bd4d6
jacky jean louis <j.st.jeanlouis@gmail.com>
2020-02-13 19:43:41 +0100

    Jacky

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,2 +1,4 @@
 class Auction < ApplicationRecord
+  belongs_to :user
+  belongs_to :sound
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :sounds, dependent: :destroy
+  has_many :auctions, dependent: :destroy
+
+
   validates :email, presence: true
   validates :encrypted_password, presence: true
 end

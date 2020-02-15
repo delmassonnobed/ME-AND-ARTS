1a453b4c265c18b007fcc4ee435afe2c68b8f7ca
Erika Alvarez <8erika7@gmail.com>
2020-02-15 11:37:17 +0100

    create bid

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -4,9 +4,26 @@ class AuctionsController < ApplicationController
   end
 
   def show
-    @auction = Auction.find(id=params[:id])
+    @auction = Auction.find(params[:id])
     @user = User.find_by_id(@auction.user_id)
     @timer = @auction.created_at
+  end
 
+  def create
+    @auction = Auction.new(auction_params)
+    @auction.user_id = current_user.id
+    @sound = Sound.find(params[:sound_id])
+    @auction.sound_id = @sound.id
+    @auction.save
+    if @auction.save
+      redirect_to sound_path(@sound)
+    else
+      render action: "show", controller: "sounds"
+    end
+  end
+
+
+  def auction_params
+    params.require(:auction).permit(:amount)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'sounds#index'
-  resources :sounds do
+  resources :sounds, only: [:show, :index, :new, :create] do
     resources :auctions, only: [:new, :create]
    end
    resources :auctions, only: [:index, :show]

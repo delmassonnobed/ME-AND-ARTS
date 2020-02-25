55ac58c9aca8eb139d16f8527621d18bbdb1b8f4
jeremy bultel <jerem.bultel@gmail.com>
2020-02-25 17:35:58 +0100

    ready to test image in seed v2

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -6,7 +6,7 @@ class Sound < ApplicationRecord
   validates :genre, presence: true
 
   has_one_attached :mp3_file
-  has_one_attached :image
+  has_one_attached :photo
 
   has_many :auctions, dependent: :destroy
 
